f93074977d7d3381eb4ef1ea9e76d5f1daafb4e0
Ahila-pro <ahilaasokan2025@gmail.com>
2025-11-08 15:50:35 +0530

    aks cluster creation code added

--- aks-cluster.tf
+++ aks-cluster.tf
@@ -1,4 +1,25 @@
-resource "azurerm_resource_group" "example_rg" {
-  name     = "test-tf-rgp-aks"
-  location = "East US"
+resource "azurerm_kubernetes_cluster" "default" {
+  name                = "aks-test-001"
+  location            = "eastus"
+  resource_group_name = "lgbmegathon"
+  dns_prefix          = "dns-k8s-test"
+  kubernetes_version  = "1.27.9"
+
+  default_node_pool {
+    name            = "testnodepool"
+    node_count      = 2
+    vm_size         = "Standard_D2_v2"
+    os_disk_size_gb = 30
+  }
+
+  service_principal {
+    client_id     = "client-id"
+    client_secret = "client-secret"
+  }
+
+  role_based_access_control_enabled = true
+
+  tags = {
+    environment = "dev"
+  }
 }
